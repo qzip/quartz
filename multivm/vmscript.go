64c31c2cd403790c1e094c59a07d12c68c14dbf2
innomon <tech@ashishbanerjee.com>
2023-08-03 11:37:20 +0530

    vmscript added

--- multivm/vmscript.go
+++ multivm/vmscript.go
@@ -11,6 +11,7 @@ Hash, time stamp, vm type, file name, content
 
 type VM string
 
+/*
 const (
 	JS      VM = "js"       // https://github.com/dop251/goja
 	WASM    VM = "wasm"     // https://github.com/tetratelabs/wazero
@@ -25,4 +26,33 @@ type VMScipt struct {
 	VMtype   VM
 	Tmstamp  time.Time
 	Content  []byte
+}
+*/
+
+const (
+	JS      Ftype = "js"       // https://github.com/dop251/goja
+	WASM    Ftype = "wasm"     // https://github.com/tetratelabs/wazero
+	CEL     Ftype = "cel"      //https://github.com/google/cel-go
+	JOKER   Ftype = "joker"    // https://github.com/candid82/joker
+	GNO     Ftype = "gno"      // https://github.com/gnolang/gno/tree/master/gnovm
+	SKYLARK Ftype = "starlark" // https://github.com/google/starlark-go/blob/master/doc/spec.md
+	JPG     Ftype = "jpg"
+	HTML    Ftype = "html"
+	PDF     Ftype = "pdf"
+	JSON    Ftype = "json"
+	XML     Ftype = "xml"
+)
+
+// Genralized to include other file types like jpg,index...
+
+type Ftype string
+
+const ()
+
+type FileContent struct {
+	Path      string
+	Namespace string
+	FileType  Ftype
+	Tmstamp   time.Time
+	Content   []byte
 }

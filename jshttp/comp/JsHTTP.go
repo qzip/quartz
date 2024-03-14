a1ffde58e4cc81e637d0339c81291a86ea12a7d6
innomon <tech@ashishbanerjee.com>
2024-03-14 11:18:47 +0530

    Add JsFuncInstaller interface and implement installHelpers function

--- jshttp/comp/JsHTTP.go
+++ jshttp/comp/JsHTTP.go
@@ -2,6 +2,7 @@ package comp
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -23,8 +24,13 @@ type JsHandler struct {
 }
 
 type JsHandlerParam struct {
-	Function string `json:"function"` // <Function Name>(Request , Response)
-	Script   string `json:"script"`
+	Function       string   `json:"function"` // <Function Name>(Request , Response)
+	Script         string   `json:"script"`
+	JsInstallFuncs []string `json:"jsInstFuncs"` // list of JS functions installers
+}
+
+type JsFuncInstaller interface {
+	Install(vm *goja.Runtime) error
 }
 
 // ServeHTTP implements http.Handler
@@ -38,6 +44,12 @@ func (jsh *JsHandler) ServeHTTP(req http.ResponseWriter, res *http.Request) {
 		http.Error(req, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	if err := jsh.installHelpers(vm); err != nil {
+		http.Error(req, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// install the helpers
+
 	serv, ok := goja.AssertFunction(vm.Get(jsh.Params.Function))
 	if !ok {
 		err := (jsh.Params.Function + " not found in" + jsh.Params.Script)
@@ -53,6 +65,22 @@ func (jsh *JsHandler) ServeHTTP(req http.ResponseWriter, res *http.Request) {
 
 }
 
+func (jsh *JsHandler) installHelpers(vm *goja.Runtime) error {
+	for _, f := range jsh.Params.JsInstallFuncs {
+		hlp := jsh.Ctx.Value(f)
+		if hlp == nil {
+			return fmt.Errorf("helper %v not found", f) // Fix: Changed "Helper" to "helper"
+		}
+		helper, ok := hlp.(JsFuncInstaller)
+		if !ok {
+			return fmt.Errorf("helper %v is not of type JsFuncInstaller", f) // Fix: Changed "Helper" to "helper"
+		}
+		if err := helper.Install(vm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 func (jsh *JsHandler) ExtractJs() error {
 	prog, err := os.ReadFile(jsh.Params.Script)
 	if err != nil {

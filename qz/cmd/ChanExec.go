9f5447dc0d55a52baf9a5c454d0b8a2c3df3f871
innomon <tech@ashishbanerjee.com>
2024-03-16 11:36:48 +0530

    chan exec

--- qz/cmd/ChanExec.go
+++ qz/cmd/ChanExec.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"qz/commands"
 	"reflect"
+	"sync"
 )
 
 // ChanExec implements commands.Commander
@@ -39,6 +40,73 @@ func (e *ChanExec) Exec(ctx context.Context, cfg map[string]interface{}, errCh c
 		errCh <- commands.NewFatalError("cmd.ChanExec.Exec: helper not of type commands.Source")
 		return
 	}
+
+	if hlp, err := e.getHelper(ctx, cfg, sinkKey); err != nil {
+		errCh <- commands.NewFatalError(err.Error())
+		return
+	} else if e.sink, ok = hlp.(commands.Sink); !ok {
+		errCh <- commands.NewFatalError("cmd.ChanExec.Exec: helper not of type commands.Sink")
+		return
+	}
+
+	// deserialize the transformer array, transformers are optional
+	tarrKey, ok := cfg[transformersKey]
+	if ok {
+		tarr, ok := tarrKey.([]string)
+		if !ok {
+			errCh <- commands.NewFatalError("cmd.ChanExec.Exec: transformer key [" + transformersKey + "] string array expected")
+			return
+		}
+		e.transformers = make([]commands.Transformer, len(tarr))
+		for i, t := range tarr {
+
+			if handler, err := e.getHelper(ctx, cfg, t); err != nil {
+				errCh <- commands.NewFatalError("cmd.ChanExec.Exec: helper not of type commands.Transformer array")
+				return
+			} else if e.transformers[i], ok = handler.(commands.Transformer); !ok {
+				errCh <- commands.NewFatalError("cmd.ChanExec.Exec: helper not of type commands.Transformer array")
+				return
+			}
+
+		}
+	}
+	e.spinOff(ctx, errCh)
+}
+
+func (e *ChanExec) spinOff(ctx context.Context, errCh chan error) {
+
+	schan, err := e.source.Chan(ctx)
+	if err != nil {
+		errCh <- commands.NewFatalError(err.Error())
+		return
+	}
+
+	for _, t := range e.transformers {
+		schan, err = t.Chan(ctx, schan)
+		if err != nil {
+			errCh <- commands.NewFatalError(err.Error())
+			return
+		}
+	}
+	if err = e.sink.Chan(ctx, schan); err != nil {
+		errCh <- commands.NewFatalError(err.Error())
+		return
+	}
+
+	var wg sync.WaitGroup
+	f := func(p commands.Pipeline) {
+		p.Process(ctx)
+		wg.Done()
+	}
+	wg.Add(2)
+	go f(e.source)
+	go f(e.sink)
+	for _, t := range e.transformers {
+		wg.Add(1)
+		go f(t)
+	}
+	wg.Wait()
+
 }
 
 func (e *ChanExec) getHelper(ctx context.Context, cfg map[string]interface{}, key string) (helper interface{}, err error) {

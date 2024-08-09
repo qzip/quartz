1695e364deac405228b07b9b0ee7fd635487ab9c
innomon <tech@ashishbanerjee.com>
2024-08-09 12:51:02 +0530

    product specs

--- imle/catalog/catalog.go
+++ imle/catalog/catalog.go
@@ -1,19 +1,25 @@
 package catalog
 
-//Node  in the Catalog DAG (Distributed Acyclic Graph)
+// Node  in the Catalog DAG (Distributed Acyclic Graph)
 type Node struct {
-	ID         string `json:"id"`
-	UUID       string `json:"uuid,omitempty"`
-	ParentUUID string `json:"parent,omitempty"`
-	Namespace  string `json:"ns,omitempty"`
-	Title      string `json:"title"`
-	URL        string `json:"url,omitempty"`
-	Tags       []Tag  `json:"tag,omitempty"`
+	W3cDid       string    `json:"w3cdid"`
+	ParentW3cDid string    `json:"parent,omitempty"`
+	Title        string    `json:"title"`
+	URL          string    `json:"url,omitempty"`
+	Tags         []Tag     `json:"tag,omitempty"`
+	Specs        ProdSpecs `json:"specs,omitempty"`
 }
 
-//Tag help in searching & filtering
+// Tag help in searching & filtering
 type Tag struct {
 	Namespace string `json:"ns,omitempty"`
 	Key       string `json:"key"`
 	Value     string `json:"value,omitempty"`
+}
+
+type ProdSpecs struct {
+	Namespace        string                 `json:"ns"` // <domain>:<schema> , the last : defines schema
+	W3cDid           string                 `json:"w3cdid"`
+	ProdAttributes   map[string]interface{} `json:"attributes,omitempty"`
+	ComponentsW3cDid []string               `json:"components,omitempty"`
 }
